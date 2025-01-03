bb7fa28252b7b5cab03600b39130cb0997f35d03
Aziz Jlassi <mohamedaziz.jlassi@sesame.com.tn>
2025-01-03 17:51:08 +0100

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
+import { StatistiqueComponent } from './statistique/statistique.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { SupportComponent } from './support/support.component';  // Import the SupportComponent
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'statistique', component: StatistiqueComponent },
+  { path: 'feedback', component: FeedbackComponent },
+  { path: 'support', component: SupportComponent }  // Add the Support route here
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
